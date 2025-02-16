b71c89fa77445eb08644280f5162b48eb130d61e
josee022 <jose.mondelo@iesdonana.org>
2025-02-16 18:28:52 +0100

    arreglos

--- src/app/components/animacion.js
+++ src/app/components/animacion.js
@@ -1,4 +1,10 @@
-import { Player } from "@lottiefiles/react-lottie-player";
+"use client";
+import dynamic from "next/dynamic";
+
+const Player = dynamic(
+  () => import("@lottiefiles/react-lottie-player").then((mod) => mod.Player),
+  { ssr: false }
+);
 
 const LottieAnimation = () => {
   return (

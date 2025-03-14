87a0e2b08af74e31acc8444198a0eb6df1233ce7
josee022 <jose.mondelo@iesdonana.org>
2025-03-14 17:17:45 +0100

    proyecto gestor tareas add

--- src/app/layout.js
+++ src/app/layout.js
@@ -43,7 +43,7 @@ export default function RootLayout({ children }) {
                 "Inicio",
                 "Sobre Mí",
                 "Habilidades",
-                "Destacado",
+                "Proyectos",
                 "Contacto",
               ].map((item, index) => (
                 <motion.li
@@ -84,7 +84,7 @@ export default function RootLayout({ children }) {
                 "Inicio",
                 "Sobre Mí",
                 "Habilidades",
-                "Destacado",
+                "Proyectos",
                 "Contacto",
               ].map((item, index) => (
                 <motion.li
